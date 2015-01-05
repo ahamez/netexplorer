56c5ef6fca69fdb4e2a020de98811447a054c51e
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-01-05 19:10:13 +0100

    Documentation & reserve memory

--- ntx/detail/normalize_utf8.cc
+++ ntx/detail/normalize_utf8.cc
@@ -1,5 +1,3 @@
-#include <stdexcept>
-
 #include <unilib/uninorms.h>
 #include <unilib/utf8.h>
 
@@ -15,16 +13,20 @@ normalize_utf8(const std::string& s)
 {
   auto copy = s;
   auto tmp = std::u32string{};
+  tmp.reserve(s.size());
   ufal::unilib::utf8::decode(copy, tmp);
   ufal::unilib::uninorms::nfc(tmp);
   ufal::unilib::utf8::encode(tmp, copy);
   return copy;
 }
 
+/*------------------------------------------------------------------------------------------------*/
+
 std::string&
 normalize_utf8(std::string&& s)
 {
   auto tmp = std::u32string{};
+  tmp.reserve(s.size());
   ufal::unilib::utf8::decode(s, tmp);
   ufal::unilib::uninorms::nfc(tmp);
   ufal::unilib::utf8::encode(tmp, s);
--- ntx/detail/normalize_utf8.hh
+++ ntx/detail/normalize_utf8.hh
@@ -8,10 +8,14 @@ namespace detail {
 /*------------------------------------------------------------------------------------------------*/
 
 /// @internal
+/// @brief Normalize an UTF-8 string.
 std::string
 normalize_utf8(const std::string&);
 
+/*------------------------------------------------------------------------------------------------*/
+
 /// @internal
+/// @brief Normalize an UTF-8 temporary string.
 std::string&
 normalize_utf8(std::string&&);
 
